dc079da6556786c2c9d93a69ca7d9095b74dc01b
LouiseWhite30 <louise.white30@nhs.net>
2021-09-26 15:32:09 +0100

    file upload tweaks

--- app/views/current/file-upload/_routes.js
+++ app/views/current/file-upload/_routes.js
@@ -5,6 +5,50 @@ const axios = require('axios');
 
 // Add your routes here - above the module.exports line
 
+
+// Do you live in the UK? - uk-address.html
+router.post('/ukAddress', function (req, res) {
+  var ukAddress = req.session.data['uk-address']
+  if (ukAddress == "yes") {
+    res.redirect('address-lookup')
+  }
+  else if (ukAddress == "no") {
+    res.redirect('address-eu')
+  }
+  else {
+    res.redirect('uk-address')
+  }
+})
+
+// Do you need to provide evidence of your right to reside in the UK? - provide-rtr-evid.html
+router.post('/provideRTR', function (req, res) {
+  var provideRTR = req.session.data['provide-RTR']
+  if (provideRTR == "yes") {
+    res.redirect('upload-reside')
+  }
+  else if (provideRTR == "no") {
+    res.redirect('provide-res-uk-evid')
+  }
+  else {
+    res.redirect('provide-rtr-evid')
+  }
+})
+
+// Do you need to provide evidence of your UK residency? - provide-res-uk-evid.html
+router.post('/provideUkRes', function (req, res) {
+  var provideUkRes = req.session.data['provide-uk-res']
+  if (provideUkRes == "yes") {
+    res.redirect('upload-res-uk')
+  }
+  else if (provideUkRes == "no") {
+    res.redirect('additional-info-1')
+  }
+  else {
+    res.redirect('provide-res-uk-evid')
+  }
+})
+
+
 // Do you want to add another? (evidence for Right to Reside in UK) 
 // - upload-reside-another.html, upload-reside-another2.html
 router.post('/addEvidRight', function (req, res) {
@@ -13,7 +57,8 @@ router.post('/addEvidRight', function (req, res) {
     res.redirect('upload-reside2')
   }
   else if (addEvidRight == "No") {
-    res.redirect('upload-res-uk')
+    // res.redirect('upload-res-uk')
+    res.redirect('provide-res-uk-evid')
   }
   else {
     res.redirect('upload-reside-another')
@@ -28,7 +73,8 @@ router.post('/addEvidResUK', function (req, res) {
     res.redirect('upload-res-uk2')
   }
   else if (addEvidResUK == "No") {
-    res.redirect('cya')
+    // res.redirect('cya')
+    res.redirect('additional-info')
   }
   else {
     res.redirect('upload-res-uk-another')
