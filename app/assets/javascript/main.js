40f61441a69d95b7b254e1dffc408e35f36fa45a
elisa-nhsbsa <elisa.lazar@nhs.net>
2022-01-25 16:28:19 +0000

    Dual Nationality In Progress

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -3,3 +3,37 @@
 accessibleAutocomplete.enhanceSelectElement({
   selectElement: document.querySelector('#location-picker')
 })
+
+accessibleAutocomplete.enhanceSelectElement({
+  selectElement: document.querySelector('#location-picker-2')
+})
+
+accessibleAutocomplete.enhanceSelectElement({
+  selectElement: document.querySelector('#location-picker-3')
+})
+
+accessibleAutocomplete.enhanceSelectElement({
+  selectElement: document.querySelector('#location-picker-4')
+})
+
+accessibleAutocomplete.enhanceSelectElement({
+  selectElement: document.querySelector('#location-picker-5')
+})
+
+accessibleAutocomplete.enhanceSelectElement({
+  selectElement: document.querySelector('#location-picker-6')
+})
+
+accessibleAutocomplete.enhanceSelectElement({
+  selectElement: document.querySelector('#location-picker-7')
+})
+
+
+
+accessibleAutocomplete.enhanceSelectElement({
+  selectElement: document.querySelector('#efta-location-picker-9')
+})
+
+accessibleAutocomplete.enhanceSelectElement({
+  selectElement: document.querySelector('#other-location-picker-10')
+})
