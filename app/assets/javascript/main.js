00cf96be641f1c929a4ae199096a682d6de87fa2
Darren Robson <darren.robson2@nhs.net>
2022-02-09 16:49:50 +0000

    Check if element exists before adding autocomplete JS

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,25 +1,37 @@
 // ES6 or Vanilla JavaScript
 
-accessibleAutocomplete.enhanceSelectElement({
-  selectElement: document.querySelector('#other-location-picker')
-})
+if(document.querySelector('#other-location-picker')) {
+  accessibleAutocomplete.enhanceSelectElement({
+    selectElement: document.querySelector('#other-location-picker')
+  })
+}
 
-accessibleAutocomplete.enhanceSelectElement({
-  selectElement: document.querySelector('#eu-location-picker')
-})
+if(document.querySelector('#eu-location-picker')) {
+  accessibleAutocomplete.enhanceSelectElement({
+    selectElement: document.querySelector('#eu-location-picker')
+  })
+}
 
-accessibleAutocomplete.enhanceSelectElement({
-  selectElement: document.querySelector('#efta-location-picker-3')
-})
+if(document.querySelector('#efta-location-picker-3')) {
+  accessibleAutocomplete.enhanceSelectElement({
+    selectElement: document.querySelector('#efta-location-picker-3')
+  })
+}
 
-accessibleAutocomplete.enhanceSelectElement({
-  selectElement: document.querySelector('#location-picker-1')
-})
+if(document.querySelector('#location-picker-1')) {
+  accessibleAutocomplete.enhanceSelectElement({
+    selectElement: document.querySelector('#location-picker-1')
+  })
+}
 
-accessibleAutocomplete.enhanceSelectElement({
-  selectElement: document.querySelector('#location-picker-2')
-})
+if(document.querySelector('#location-picker-2')) { 
+  accessibleAutocomplete.enhanceSelectElement({
+    selectElement: document.querySelector('#location-picker-2')
+  })
+}
 
-accessibleAutocomplete.enhanceSelectElement({
-  selectElement: document.querySelector('#location-picker-3')
-})
+if(document.querySelector('#location-picker-3')) {
+  accessibleAutocomplete.enhanceSelectElement({
+    selectElement: document.querySelector('#location-picker-3')
+  })
+}
