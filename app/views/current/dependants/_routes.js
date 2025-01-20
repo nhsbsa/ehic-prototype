3643bb95a5e9bb32c390a0a01eb667631155c0ee
Douglas Caven <douglas.caven@nhsbsa.nhs.uk>
2025-01-20 15:11:21 +0000

    add dependants

--- app/views/current/dependants/_routes.js
+++ app/views/current/dependants/_routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const axios = require('axios');
+const { info } = require('sass');
 
 // Add your routes here - above the module.exports line
 
@@ -15,8 +16,35 @@ router.post('/newApplication', function (req, res) {
   else if (newApplication == "replacement") {
     res.redirect('next-steps')
   }
+  else if (newApplication == "dependant") {
+    res.redirect('next-steps')
+  }
   else {
     res.redirect('new-application')
+  }
+})
+
+// Next steps for adding a dependant - next-steps.html
+router.post('/nextSteps', function (req, res) {
+  res.redirect('full-name') 
+})
+
+// Full name for adding a dependant - full-name.html
+router.post('/fullName', function (req, res) {
+  res.redirect('dob') 
+})
+
+// UK address for adding a dependant - uk-address.html
+router.post('/ukAddress', function (req, res) {
+  var ukAddress = req.session.data['uk-address']
+  if (ukAddress == "yes") {
+    res.redirect('postcode')
+  }
+  else if (ukAddress == "no") {
+    res.redirect('country')
+  }
+  else {
+    res.redirect('uk-address')
   }
 })
 
